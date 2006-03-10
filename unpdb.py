9f709b87e57e8a5359d693f0a98a8ea532feabb2
bobofdoom <bobofdoom@4cf8e309-b20d-0410-9cb3-b925f7cd04b4>
2006-03-10 23:14:55 +0000

    Reduced output verbosity.
    git-svn-id: file:///home/ato/tmp/hackndev-svn/trunk/tools@203 4cf8e309-b20d-0410-9cb3-b925f7cd04b4

--- unpdb.py
+++ unpdb.py
@@ -7,7 +7,7 @@ import sys, struct
 
 def main():
 	if len(sys.argv) != 3:
-		print 'Usage: %s <pdb file> <out file>' % sys.argv[0]
+		sys.stderr.write('Usage: %s <pdb file> <out file>\n' % sys.argv[0])
 		return
 		
 	fn = sys.argv[1]
@@ -16,18 +16,14 @@ def main():
 	f.seek(76)
 	
 	num_rec = struct.unpack('>H', f.read(2))[0]
-	print num_rec
 	toc = []
 
 	for i in range(num_rec):
 		toc.append(struct.unpack('>II', f.read(8))[0])
-		print toc[-1]
 
 	for i in range(num_rec):
 		f.seek(toc[i] + 4)
 		blksize = struct.unpack('>I', f.read(4))[0]
-		print blksize
-		fo.write( f.read(blksize) )		
 	
 	f.close()
 	if fo is not sys.stdout: fo.close()
