e839dd4a232a362e4c9b1b9ee783bcf22f63f4ee
bobofdoom <bobofdoom@4cf8e309-b20d-0410-9cb3-b925f7cd04b4>
2006-03-11 00:40:24 +0000

    Added option to concatenate ROM image with header.
    git-svn-id: file:///home/ato/tmp/hackndev-svn/trunk/tools@206 4cf8e309-b20d-0410-9cb3-b925f7cd04b4

--- makecafe.py
+++ makecafe.py
@@ -7,32 +7,38 @@ from sys import stdout, stderr
 HEADER = 0xacecafe0
 DEF_OFFSET = 134080    # in sectors (1 sector = 512 bytes)
 DEF_LENGTH = 20479778  # in bytes
+BLOCKSIZE = 512	       # for cancatenate option
 
 def usage():
-	stderr.write('Usage: %s [-o offset] [-l length] [filename]\n' % sys.argv[0])
+	stderr.write('Usage: %s [-o offset] [-l length] [romfile]\n' % sys.argv[0])
 	stderr.write('Generate a LD rom partition header.\n\n')
 	stderr.write(' -o [offset]     image offset in sectors (defaults to %d)\n' % DEF_OFFSET)
 	stderr.write(' -l [length]     image length in bytes (defaults to size of given filename)\n')
+	stderr.write(' -c              concatenate header with rom image\n')
 
 
 def main():
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], 'o:l:')
+		opts, args = getopt.getopt(sys.argv[1:], 'o:l:c')
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
 		
 	offset = DEF_OFFSET
 	length = DEF_LENGTH
+	concat = 0
 
 	if args:
-		length = os.path.getsize(args[0])
+		filename = args[0]
+		length = os.path.getsize(filename)
 
 	for o, a in opts:
 		if o == '-o':
 			offset = int(a)
 		elif o == '-l':
 			length = int(a)
+		elif o == '-c':
+			concat = 1
 	
 	
 	# first sector
@@ -45,6 +51,14 @@ def main():
 	# image length
 	stdout.write( pack('<IIII', length, 0, 0, 0) )
 	stdout.write('\0' * 0x1f0)
+
+	if concat and filename:
+		f = file(filename, 'rb')
+		while 1:
+			data = f.read(BLOCKSIZE)
+			if data == '': break
+			stdout.write(data)
+		del data
 
 if __name__ == '__main__': main()
 
